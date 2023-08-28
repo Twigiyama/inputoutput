f4988e362926f1a3d4c1faf2e5aa1c0d7a1bf12c
Asitha Rodrigo <asitharodrigo@Asithas-MacBook-Pro-2023.local>
2023-08-28 21:29:53 +0100

    Command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+if env::args().len() <= 2 {
+    println!("We need at least 2 arguments");
+    return;
+}
+
+    for (index, argument) in env::args().enumerate() {
+        println!("Argument index {index}, with argument {argument}");
+        let arg2 = env::args().nth(2).unwrap();
+        println!("Second argument is {arg2}");
+    }
+    
 }
